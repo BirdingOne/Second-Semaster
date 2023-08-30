b725272468578399bcba92c4d1dc7a2a6a59a5d6
BirdingOne <123818635+BirdingOne@users.noreply.github.com>
2023-08-30 14:08:33 +0900

    .

---
+++ game/index.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehavier
+{
+    void Start()
+    {
+        // code
+    }
+
+    void Update()
+    {
+        //code
+    }
+    
+}
